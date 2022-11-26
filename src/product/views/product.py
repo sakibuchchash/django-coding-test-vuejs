1f80361645aeaa502f3a43f02b408ecdd86fde07
unknown <mtoaha161093@bscse.uiu.ac.bd>
2022-11-26 22:06:19 +0600

    Initial Dev Commit

--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,7 @@
 from django.views import generic
+from django.views.generic import ListView
 
-from product.models import Variant
+from product.models import Variant, Product
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +12,24 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+class ProductView(ListView):
+    template_name = 'products/list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        filter_string = {}
+        print(self.request.GET)
+        for key in self.request.GET:
+            if self.request.GET.get(key):
+                filter_string[key] = self.request.GET.get(key)
+        return Product.objects.filter(**filter_string)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
+        context['request'] = ''
+        if self.request.GET:
+            context['request'] = self.request.GET['title__icontains']
         return context
